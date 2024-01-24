fbdbe5d2db29493bc7e9dded8c20233fa42661b8
machida <banzyonosite@gmail.com>
2024-01-24 22:38:39 +0900

    ドキュメントクラスの用意

---
+++ MEdit/Test/TextDocumentTest.cs
@@ -0,0 +1,31 @@
+﻿using MEdit;
+
+namespace Test {
+    public class TextDocumentTest {
+
+        [Test]
+        public void TestInsertText() {
+            var doc = new TextDocument();
+            doc.Insert(0, "test");
+            Assert.That(doc.Text, Is.EqualTo("test"));
+        }
+
+        [Test]
+        public void TestReplaceText() {
+            var doc = new TextDocument();
+            doc.Insert(0, "testtest");
+            doc.Replace(0, 4, "test", "buffer");
+            Assert.That(doc.Text, Is.EqualTo("buffertest"));
+        }
+
+        [Test]
+        public void TestDeleteText() {
+            var doc = new TextDocument();
+            doc.Insert(0, "test");
+            doc.Delete(0, 2);
+            Assert.That(doc.Text, Is.EqualTo("st"));
+        }
+
+        // todo: implement function to get offset of specified string
+    }
+}
