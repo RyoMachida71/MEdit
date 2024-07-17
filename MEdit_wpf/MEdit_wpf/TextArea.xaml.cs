409b5ed8dffd89bc53506df6b605c5bf125017a4
RyoMachida <banzyonosite@gmail.com>
2024-07-17 21:30:22 +0900

    IScrollInfoの実装

--- MEdit_wpf/MEdit_wpf/TextArea.xaml.cs
+++ MEdit_wpf/MEdit_wpf/TextArea.xaml.cs
@@ -3,6 +3,7 @@ using MEdit_wpf.Document;
 using MEdit_wpf.Layer;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -10,7 +11,7 @@ namespace MEdit_wpf {
     /// <summary>
     /// TextArea.xaml の相互作用ロジック
     /// </summary>
-    public partial class TextArea : Control, ITextArea {
+    public partial class TextArea : Control, ITextArea, IScrollInfo {
 
         private TextDocument _document;
         private SingleCaret _caret;
@@ -69,6 +70,86 @@ namespace MEdit_wpf {
 
         private void RenderCaret() {
             _caretLayer.Render(_visualText.GetCaretScreenPosition(_caret.Position), _visualText.GetSelectionScreenRects(_caret.Selection, _document));
+        }
+
+        private bool _canVerticallyScroll;
+        public bool CanVerticallyScroll { get => _canVerticallyScroll; set => _canVerticallyScroll = value; }
+
+        private bool _canHorizontallyScroll;
+        public bool CanHorizontallyScroll { get => _canHorizontallyScroll; set => _canHorizontallyScroll = value; }
+
+        public double ExtentWidth => throw new System.NotImplementedException();
+
+        public double ExtentHeight => throw new System.NotImplementedException();
+
+        public double ViewportWidth => throw new System.NotImplementedException();
+
+        public double ViewportHeight => throw new System.NotImplementedException();
+
+        public double HorizontalOffset => throw new System.NotImplementedException();
+
+        public double VerticalOffset => throw new System.NotImplementedException();
+
+        public ScrollViewer ScrollOwner { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void LineUp() {
+            throw new System.NotImplementedException();
+        }
+
+        public void LineDown() {
+            throw new System.NotImplementedException();
+        }
+
+        public void LineLeft() {
+            throw new System.NotImplementedException();
+        }
+
+        public void LineRight() {
+            throw new System.NotImplementedException();
+        }
+
+        public void PageUp() {
+            throw new System.NotImplementedException();
+        }
+
+        public void PageDown() {
+            throw new System.NotImplementedException();
+        }
+
+        public void PageLeft() {
+            throw new System.NotImplementedException();
+        }
+
+        public void PageRight() {
+            throw new System.NotImplementedException();
+        }
+
+        public void MouseWheelUp() {
+            throw new System.NotImplementedException();
+        }
+
+        public void MouseWheelDown() {
+            throw new System.NotImplementedException();
+        }
+
+        public void MouseWheelLeft() {
+            throw new System.NotImplementedException();
+        }
+
+        public void MouseWheelRight() {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetHorizontalOffset(double offset) {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetVerticalOffset(double offset) {
+            throw new System.NotImplementedException();
+        }
+
+        public Rect MakeVisible(Visual visual, Rect rectangle) {
+            throw new System.NotImplementedException();
         }
     }
 }
