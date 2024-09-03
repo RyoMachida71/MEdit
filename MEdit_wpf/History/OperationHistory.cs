a627447d7b95741af45d0c5e55d390fa2c28cf51
RyoMachida <banzyonosite@gmail.com>
2024-09-03 22:24:49 +0900

    Historyの実装

---
+++ MEdit_wpf/History/OperationHistory.cs
@@ -0,0 +1,20 @@
+﻿namespace History {
+    public class OperationHistory {
+        Stack<IUndoable> _undos;
+
+        public OperationHistory()
+        {
+            _undos = new Stack<IUndoable>();
+        }
+
+        public void Push(IUndoable operation) {
+            _undos.Push(operation);
+        }
+
+        public void Undo() {
+            if (_undos.Count == 0) return;
+            var lastOperation = _undos.Pop();
+            lastOperation.Undo();
+        }
+    }
+}
---
+++ MEdit_wpf/MEdit_Test/TestHistory/TestOperationHistory.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using History;
+using Moq;
+
+namespace MEdit_Test.TestHistory {
+    public class TestOperationHistory {
+        [Test]
+        public void TestUndo() {
+            var sb = new StringBuilder();
+            var history = new OperationHistory();
+            var mock = new Mock<IUndoable>();
+            mock.Setup(x => x.Undo()).Callback(() => sb.Append("Undo Operation."));
+
+            history.Push(mock.Object);
+            history.Undo();
+
+            Assert.That(sb.ToString(), Is.EqualTo("Undo Operation."));
+        }
+
+        [Test]
+        public void TestUndoMultipleTimes() {
+            var sb = new StringBuilder();
+            var counter = 0;
+            var history = new OperationHistory();
+            var mock = new Mock<IUndoable>();
+            mock.Setup(x => x.Undo()).Callback(() => sb.Append($"Undo Operation{++counter}."));
+
+            history.Push(mock.Object);
+            history.Push(mock.Object);
+            history.Push(mock.Object);
+            history.Undo();
+            history.Undo();
+
+            Assert.That(sb.ToString(), Is.EqualTo("Undo Operation1.Undo Operation2."));
+        }
+
+        [Test]
+        public void TestUndoWithoutAnyOperations() {
+            // don't throw any exceptions
+            new OperationHistory().Undo();
+        }
+    }
+}
